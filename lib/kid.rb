19830fd351fecce059f94b785271ff9dc2b81c46
Naya  Willis <greedybrain18@gmail.com>
2019-10-15 17:04:38 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,9 +2,9 @@ require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  include FancyDance
   
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name 
   
